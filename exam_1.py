4d3b09a15ab787fd8b298b79444ffa24ddc1df07
dplvv <polivaev04@gmail.com>
2026-05-09 02:13:51 +0300

    exam_1 Итоговая работа на ввод-вывод данных

---
+++ exam_1.py
@@ -0,0 +1,24 @@
+# z1
+print("*****************")
+print("*               *")
+print("*               *")
+print("*****************")
+
+# z2
+a = int(input())
+b = int(input())
+print("Квадрат суммы", a, "и", b, "равен", a**2 + 2 * a * b + b**2)
+print("Сумма квадратов", a, "и", b, "равна", (a + b) ** 2 - 2 * a * b)
+
+# z3
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
+
+# z4
+a = input()
+b = a + a
+c = a + a + a
+print(int(a) + int(b) + int(c))
